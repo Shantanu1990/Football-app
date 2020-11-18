d571cb76bb13adb5cc769886d38551b90a6f3ea4
ESHUCHY <shantanu.chakraborty@ericsson.com>
2020-11-19 00:56:32 +0530

    commit

--- build_script.sh
+++ build_script.sh
@@ -3,8 +3,14 @@ rm -r dist
 cd ../src/main/resources/static
 rm -r *.*
 cd ../../../../football-ui
-npm install
+if [! -d "node_modules" ]
+then
+	echo "Starting npm install"
+	npm install
+fi
+echo "npm installed running npm run"
 npm run-script build --watch
 cd ../
+echo "running mvn clean install"
 mvn clean install
 java -jar target/football-0.0.1-SNAPSHOT.jar
