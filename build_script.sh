b79bec863d31ed36914062ca3a9222bf4674de65
Shantanu.C <Shantanu.C@HIRING.PUBLICISSAPIENT.COM>
2020-11-19 13:11:25 +0000

    commit

--- build_script.sh
+++ build_script.sh
@@ -1,13 +1,13 @@
-cd football-ui
-rm -r dist
-cd ../src/main/resources/static
+#cd football-ui
+#rm -r dist
+cd /src/main/resources/static
 rm -r *.*
-cd ../../../../football-ui
-echo "Starting npm install, please wait..."
-npm install
-echo "npm installed running npm run"
-npm run-script build --watch
-cd ../
+#cd ../../../../football-ui
+#echo "Starting npm install, please wait..."
+#npm install
+#echo "npm installed running npm run"
+#npm run-script build --watch
+cd ../../../../
 echo "running mvn clean install"
 mvn clean install
 java -jar target/football-0.0.1-SNAPSHOT.jar
